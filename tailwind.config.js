4d3c0611db08ff895dcbbf6689f09c2be943e623
Juan Pablo <juanpa_roto@hotmail.com>
2023-02-25 17:53:02 -0600

    made the navbar sticky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["index.html", "/src/**/*.{html,js}"],
+  content: ["index.html", "*.{html,js}"],
   theme: {
     extend: {},
   },
